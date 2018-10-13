5be37001f45962579514ec7d69d51fb4f8391e1e
nXtremist <nXtremist@users.noreply.github.com>
2018-10-13 19:16:04 +0530

    Update QUALPREL.cpp

--- QUALPREL.cpp
+++ QUALPREL.cpp
@@ -8,24 +8,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//Quick Sort then Find
-
 void merge(long int arr[], int l, int m, int r)
 {
     int i, j, k;
     int n1 = m - l + 1;
     int n2 =  r - m;
 
-    /* create temp arrays */
     int L[n1], R[n2];
 
-    /* Copy data to temp arrays L[] and R[] */
     for (i = 0; i < n1; i++)
         L[i] = arr[l + i];
     for (j = 0; j < n2; j++)
         R[j] = arr[m + 1 + j];
 
-    /* Merge the temp arrays back into arr[l..r]*/
     i = 0; // Initial index of first subarray
     j = 0; // Initial index of second subarray
     k = l; // Initial index of merged subarray
@@ -40,16 +35,12 @@ void merge(long int arr[], int l, int m, int r)
         k++;
     }
 
-    /* Copy the remaining elements of L[], if there
-       are any */
     while (i < n1) {
         arr[k] = L[i];
         i++;
         k++;
     }
 
-    /* Copy the remaining elements of R[], if there
-       are any */
     while (j < n2) {
         arr[k] = R[j];
         j++;
@@ -57,16 +48,11 @@ void merge(long int arr[], int l, int m, int r)
     }
 }
 
-/* l is for left index and r is right index of the
-   sub-array of arr to be sorted */
 void mergeSort(long int arr[], int l, int r)
 {
     if (l < r) {
-        // Same as (l+r)/2, but avoids overflow for
-        // large l and h
         int m = l + (r - l) / 2;
 
-        // Sort first and second halves
         mergeSort(arr, l, m);
         mergeSort(arr, m + 1, r);
 
@@ -113,10 +99,10 @@ void search(long *arr, int n, int k)
     //get the item;
     int temp = arr[k - 1];
     int blog = ceil(log2(n - k));
-
+    //cout<<"\nblog:"<<blog<<"\n";
     int l = k, h = n - 1, pm, m = 0;
     while(blog--) {
-        pm = m;
+        //pm = m;
         m = (l + h) / 2;
 
         if(arr[m] >= temp)
@@ -125,23 +111,28 @@ void search(long *arr, int n, int k)
         else if(arr[m] < temp)
             h = m - 1;
 
-        //cout << "l:" << l << " h:" << h << " m:" << m << " pm:" << pm << "\n"; //Debug
+        //cout << "l:" << l << " h:" << h << " m:" << m << " blog:" << blog << "\n"; //Debug
 
-        if(m == pm)
-            break;
+        //if(m == pm)
+        //    break;
 
     }
 
-    /*if(h > l) {
-        h = l;
-        //l = h - l;
-    }*/
-
-    for (int i = h; i < n ; ++i) {
-        //cout << i;
-        if(arr[i] < temp) {
-            printf(" %d\n", i);
-            break;
+    if(h < l) {
+        for (int i = h; i < n ; ++i) {
+            //cout << i;
+            if(arr[i] < temp) {
+                printf(" %d\n", i);
+                break;
+            }
+        }
+    } else {
+        for (int i = l; i < n ; ++i) {
+            //cout << i;
+            if(arr[i] < temp) {
+                printf(" %d\n", i);
+                break;
+            }
         }
     }
 
