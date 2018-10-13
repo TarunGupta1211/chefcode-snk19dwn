b1c80d28fb947d2a08ef99fca1f5067d725eab61
nXtremist <39878289+nXtremist@users.noreply.github.com>
2018-10-13 17:40:14 +0530

    merge sort UPDATED wrong answer

--- QUALPREL.cpp
+++ QUALPREL.cpp
@@ -10,45 +10,76 @@ using namespace std;
 
 //Quick Sort then Find
 
-void swap(long *a, long *b)
-{
-    long t = *a;
-    *a = *b;
-    *b = t;
-}
-
-
-int partition (long arr[], int low, int high)
-{
-    int pivot = arr[high];    // pivot
-    int i = (low - 1);  // Index of smaller element
-
-    for (int j = low; j <= high - 1; j++) {
-        // If current element is smaller than or
-        // equal to pivot
-        if (arr[j] >= pivot) {
-            i++;    // increment index of smaller element
-            swap(&arr[i], &arr[j]);
-        }
-    }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
-}
-
-
-void sort(long arr[], int low, int high)
-{
-    if (low < high) {
-        /* pi is partitioning index, arr[p] is now
-           at right place */
-        int pi = partition(arr, low, high);
-
-        // Separately sort elements before
-        // partition and after partition
-        sort(arr, low, pi - 1);
-        sort(arr, pi + 1, high);
-    }
-}
+void merge(long int arr[], int l, int m, int r) 
+{ 
+    int i, j, k; 
+    int n1 = m - l + 1; 
+    int n2 =  r - m; 
+  
+    /* create temp arrays */
+    int L[n1], R[n2]; 
+  
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++) 
+        L[i] = arr[l + i]; 
+    for (j = 0; j < n2; j++) 
+        R[j] = arr[m + 1+ j]; 
+  
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray 
+    j = 0; // Initial index of second subarray 
+    k = l; // Initial index of merged subarray 
+    while (i < n1 && j < n2) 
+    { 
+        if (L[i] <= R[j]) 
+        { 
+            arr[k] = L[i]; 
+            i++; 
+        } 
+        else
+        { 
+            arr[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of L[], if there 
+       are any */
+    while (i < n1) 
+    { 
+        arr[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of R[], if there 
+       are any */
+    while (j < n2) 
+    { 
+        arr[k] = R[j]; 
+        j++; 
+        k++; 
+    } 
+} 
+  
+/* l is for left index and r is right index of the 
+   sub-array of arr to be sorted */
+void mergeSort(long int arr[], int l, int r) 
+{ 
+    if (l > r) 
+    { 
+        // Same as (l+r)/2, but avoids overflow for 
+        // large l and h 
+        int m = l+(r-l)/2; 
+  
+        // Sort first and second halves 
+        mergeSort(arr, l, m); 
+        mergeSort(arr, m+1, r); 
+  
+        merge(arr, l, m, r); 
+    } 
+} 
 
 void search(long *arr, int, int);
 
@@ -65,14 +96,14 @@ int main(int argc, char const *argv[])
             scanf("%ld", &arr[i]);
         }
 
-        sort(arr, 0, n - 1);
+        mergeSort(arr, 0, n - 1);
 
         //Display Array
-        /*
-        cout<<"\n";
-        for(int i = 0; i < n; i++)
-            printf("%ld ", arr[i]);
-        cout << "\n";
+        
+       // cout<<"\n";
+        //for(int i = 0; i < n; i++)
+         //   printf("%ld ", arr[i]);
+        /*cout << "\n";
         for(int i = 0; i < k - 1; i++)
             printf("  ");
         cout << "^\n\n";
